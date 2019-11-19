682934fbb0c414640d4668a6d824db9253daff93
Jenesh <jenesh.napit@gmail.com>
2019-11-19 11:06:21 -0500

    Updated app to show who joined the chat

--- public/chat.js
+++ public/chat.js
@@ -1,48 +1,70 @@
 const socket = io(`http://localhost:3000`)
 
-$('form').submit((e) => {
+$('#form-chat').submit((e) => {
     e.preventDefault();
-    const msg = $('#msg').val();
-    console.log(msg)
-
-    socket.emit('message', {
-        message: msg
-    })
-
-    return false;
+    const message = $('#msg').val();
+    const name = $('#name').text();
+    
+    socket.emit('message', { message, name })
+    $('#messages').append($('<li>').text(`${name}: ${message}`).addClass('user'));
+    $('#msg').val('');
 })
 
-let stillTyping = true;
+let isTyping = false;
+let typingMsgSent = false;
 
 $('#msg').keyup((e) => {
     const msg = e.target.value;
-    if (msg.length === 0) {
-        stillTyping = true;
+    const name = $('#name').text();
+    // Emit to server that the user is typing
+    if (msg.length > 0) {
+        isTyping = true;
+        if (isTyping && !typingMsgSent) {
+            socket.emit('typing', {
+                name: name,
+                typing: true
+            })
+            typingMsgSent = true;
+            return;
+        }
+    }
+    // Emit to server that the user stopped typing
+    if (msg.length === 0 && typingMsgSent) {
+        isTyping = false;
+        typingMsgSent = false;
         socket.emit('typing', {
-            userId: socket.id,
+            name: name,
             typing: false
         })
         return;
-    } 
-    if (stillTyping) {
-        socket.emit('typing', {
-            userId: socket.id,
-            typing: true
-        })
-        stillTyping = false;
     }
 })
 
 socket.on('message', (data) => {
-    console.log(data)
-    $('#messages').append($('<li>').text(data.message));
+    $('#messages').append($('<li>').text(`${data.name}: ${data.message}`).addClass(data.name));
 })
 
 socket.on('typing', (data) => {
-    console.log(data)
     if (data.typing) {
-        $('#typing').text(`${data.userId} is typing...`);
+        $('#typing').text(`${data.name} is typing...`);
     } else {
         $('#typing').text('');
     }
+})
+
+socket.on('connected', (data) => {
+    $('#messages').append($('<li>').text(`${data.name} is now online`).addClass('connected'));
+})
+
+$('#form-nickname').submit((e) => {
+    e.preventDefault();
+    const nickname = $('#nickname').val();
+
+    $('#name').text(nickname);
+    $('#popUp').remove();
+    socket.emit('connected', {name: nickname})
+})
+
+socket.on('disconnected', (data) => {
+    $('#messages').append($('<li>').text(`${data.name} is now offline`).addClass('disconnected'));
 })
--- server/server.js
+++ server/server.js
@@ -1,30 +1,55 @@
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
-// Init express
-const app = express()
+// Init chat server
+const chatServer = express()
+
+chatServer.use(cors())
+chatServer.use(express.static(`${path.dirname(__dirname)}/public`))
+
+/*
+// Init regular server
+const app = express();
 
 app.use(cors())
 app.use(express.static(`${path.dirname(__dirname)}/public`))
 
-const server = app.listen(3000, (err) => {
-    console.clear()
-    console.log('Port: 3000')
+// Regular routes
+
+app.get('/', (req, res) => {
+    res.redirect('/login')
+})
+
+app.get('/login', (req, res) => {
+    console.log('Working /')
+    res.json({msg: 'Login'})
+})
+
+app.listen(5000, () => {
+    console.log(`Server live at http://localhost:5000`)
+})
+*/
+
+// Socket Stuff
+const server = chatServer.listen(3000, (err) => {
+    // console.clear()
+    console.log('Socket at port: 3000')
 })
 
 // Init socket
 const io = require('socket.io')(server)
 
-
-app.get('/', (req, res) => {
+chatServer.get('/', (req, res) => {
+    console.log('Working chatroom')
     res.sendFile(`${path.dirname(__dirname)}/public/index.html`)
 })
 
 io.on('connection', (socket) => {
-    console.log('Socket connected: ', socket.id)
+    // console.log('Socket connected: ', socket.id)
 
-    socket.on('disconnect', () => {
-        console.log('Disconnecting user id: ', socket.id)
+    socket.on('disconnect', (data) => {
+        console.log(`Disconnecting user ${data}`);
+        io.emit('disconnected', data);
     })
 
     socket.on('message', (data) => {
@@ -33,7 +58,19 @@ io.on('connection', (socket) => {
     })
 
     socket.on('typing', (data) => {
-        console.log(`${data.userId} is typing...`)
+        console.log(`${data.name} is typing...`)
         socket.broadcast.emit('typing', data)
     })
+
+    socket.on('connected', (data) => {
+        console.log(`User ${data.name} is online.`)
+        socket.broadcast.emit('connected', data);
+    })
 })
+
+// io.on('disconnect', (socket) => {
+//     socket.on('disconnected', (data) => {
+//         console.log(`User ${data.name} is offline.`)
+//         socket.broadcast.emit('disconnected', data);
+//     })
+// })
